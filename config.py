9567e673dc8b386170f161a50623eaedc8bee451
Armando Jouhayerk <75504310+ArmandoJouhayerk@users.noreply.github.com>
2026-09-03 17:08:40 -0600

    Change user and password in config.py
    Updated user credentials and file paths for configuration.

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 class Config:
 
     # Configuración de login y descarga
-    USUARIO = "monitor"
+    USUARIO = "Usuario"
 
-    PASSWORD = "DASH123*"
+    PASSWORD = "Contraseña"
 
     URL_KIBANA = "https://172.16.17.55:5601"
 
     CARPETA_DESCARGAS = (
-        r"C:\Users\jose.mendez\OneDrive - Scontinuidad Latam SA de CV\Documentos\S4Automation\Reportes_S4"
+        r"C:\Users\tu_usuario\OneDrive - Scontinuidad Latam SA de CV\Documentos\S4Automation\Reportes_S4"
     )
 
     ARCHIVO_LOG = (
-        r"C:\Users\jose.mendez\OneDrive - Scontinuidad Latam SA de CV\Documentos\S4Automation\Reporte_S4.log"
+        r"C:\Users\tu_usuario\OneDrive - Scontinuidad Latam SA de CV\Documentos\S4Automation\Reporte_S4.log"
     )
 
